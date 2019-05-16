bd41835355abf69e5e3f5a6086d295fe76cd89f7
Vu <pvu@ehs.com>
2019-05-16 11:49:35 -0400

    deploy the latest dll, update .netcore to 2.1, update readme

--- PowerScript.ps1
+++ PowerScript.ps1
@@ -25,3 +25,22 @@ Foreach-Object {
 
     Invoke-Expression "$command"
 }
+
+Get-ChildItem $schemaPath -Filter *.yaml | 
+Foreach-Object {
+    $TextInfo = (Get-Culture).TextInfo
+
+    $controllerName = $TextInfo.ToTitleCase($_.BaseName);
+
+    $controllerFileName = $controllerName + "Controller.cs"
+
+    $inputPath = $_.DirectoryName + '\' + $_.Name;
+
+
+    $command = 'dotnet ' + $currentPath + '/nswag_dll/dotnet-nswag.dll swagger2cscontroller /input:' + $inputPath + ' /namespace:VelocityEhs.Service.InternalApi.Ehs.InstanceApi.Controllers /output:' + $currentPath + '/Controllers/' + $controllerFileName + ' /UseLiquidTemplates:true /operationGenerationMode:MultipleClientsFromFirstTagAndPathSegments /ControllerBaseClass:"Microsoft.AspNetCore.Mvc.Controller"';
+    
+    Write-Host "Found schema " $_.Name
+    Write-Host "Generating " $controllerFileName " ..."
+
+    Invoke-Expression "$command"
+}
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 ## Follow this step to generate controller code from api schema
 
 - Clone this and put it under **C:\Source** or **C:\Src**
-- Make sure that you have .netcore 2.2 installed in the system.
+- Make sure that you have .netcore 2.1 installed in the system.
 - Export your api schema as json files and place them in 'Schema' folder.
 - Rename your schema to the controller that you want to generate
   > **Example:**  location.json will generate LocationController.cs, user.json will generate UserController.cs
